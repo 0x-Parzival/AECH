b6d5e2f9700dd3b633441356fb7f4beb709beaa4
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-04-30 14:33:18 +0530

    Update comm.go

--- blockplain/crosschain/comm.go
+++ blockplain/crosschain/comm.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
-func CrossChainTx(fromChain, toChain int, data string) {
-	fmt.Printf("Cross-chain Tx: [%d] -> [%d]: %s\n", fromChain, toChain, data)
+// CrossChainTx represents a transaction moving from one chain to another.
+type CrossChainTx struct {
+	FromChain int
+	ToChain   int
+	Data      string
+}
+
+// Send simulates sending a cross-chain transaction.
+func (tx CrossChainTx) Send() {
+	fmt.Printf("Cross-chain Tx: [%d] -> [%d]: %s\n", tx.FromChain, tx.ToChain, tx.Data)
+}
+
+// NewCrossChainTx creates a new cross-chain transaction instance.
+func NewCrossChainTx(from, to int, data string) CrossChainTx {
+	return CrossChainTx{
+		FromChain: from,
+		ToChain:   to,
+		Data:      data,
+	}
 }
