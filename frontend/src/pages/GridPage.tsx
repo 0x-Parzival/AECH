ed1757b8e5c060ffcda9d7296ec0226d97c8a809
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-05-03 15:42:54 +0530

    Create GridPage.tsx

---
+++ frontend/src/pages/GridPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GridPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Grid Page</h1>
+      {/* Add the interactive 2D grid visualization here */}
+    </div>
+  );
+};
+
+export default GridPage;
