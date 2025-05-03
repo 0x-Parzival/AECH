5325cbad7da5a5af4843d1e96a0ae465bdc83016
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-05-03 15:49:47 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GridPage from './pages/Grid';
+import AddBlockPage from './pages/AddBlock';
+import BlockDetailPage from './pages/BlockDetail';
+import SimulateTxPage from './pages/Transactions';
+import DescriptionPage from './pages/Description';
+import HomePage from './pages/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/grid" element={<GridPage />} />
+        <Route path="/add" element={<AddBlockPage />} />
+        <Route path="/block/:id" element={<BlockDetailPage />} />
+        <Route path="/simulate" element={<SimulateTxPage />} />
+        <Route path="/description" element={<DescriptionPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
