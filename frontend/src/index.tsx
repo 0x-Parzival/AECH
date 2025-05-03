9ea12811f65f05e249ec12761ba381cd03305cfb
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-05-03 15:42:11 +0530

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GridPage from './pages/GridPage';
+import AddBlockPage from './pages/AddBlockPage';
+import BlockDetailPage from './pages/BlockDetailPage';
+import SimulateTxPage from './pages/SimulateTxPage';
+import DescriptionPage from './pages/DescriptionPage';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<GridPage />} />
+        <Route path="/add" element={<AddBlockPage />} />
+        <Route path="/block/:id" element={<BlockDetailPage />} />
+        <Route path="/simulate" element={<SimulateTxPage />} />
+        <Route path="/description" element={<DescriptionPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
