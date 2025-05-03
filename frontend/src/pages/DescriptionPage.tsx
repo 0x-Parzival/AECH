4aa4d144ef734eadf67cb6086bd79a038bacc086
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-05-03 15:45:04 +0530

    Create DescriptionPage.tsx

---
+++ frontend/src/pages/DescriptionPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DescriptionPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Description of AECH and Block Plain</h1>
+      {/* Description of your blockchain system */}
+    </div>
+  );
+};
+
+export default DescriptionPage;
