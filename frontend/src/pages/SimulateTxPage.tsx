31d2f3a6697bb33ac2522213d23ae2024f52ad58
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2025-05-03 15:44:37 +0530

    Create SimulateTxPage.tsx

---
+++ frontend/src/pages/SimulateTxPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SimulateTxPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Simulate Transaction Page</h1>
+      {/* Interface for simulating transactions */}
+    </div>
+  );
+};
+
+export default SimulateTxPage;
